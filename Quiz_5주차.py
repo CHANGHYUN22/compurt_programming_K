42974e73838a73d9e2669ad2e784d60f09f658c3
CHANGHYUN22 <144446900+CHANGHYUN22@users.noreply.github.com>
2023-10-06 21:02:59 +0900

    Update Quiz_5주차.py

--- Quiz_5주차.py
+++ Quiz_5주차.py
@@ -1,8 +1,8 @@
 def 구구단(a):
-    for x in range(1, a):
+    for x in range(1, a+1):
         print("------["+str(x)+"단]------")
-        for y in range(1, a):
+        for y in range(1, a+1):
             print(x, "X", y, "=", x*y)
 
 b = input("몇 단까지 출력할까요")
-구구단(int(b)+1)
+구구단(int(b))
