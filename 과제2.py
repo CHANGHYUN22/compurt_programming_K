5b129787751a03cdee9e44c46d15b0e11fae36e7
CHANGHYUN22 <144446900+CHANGHYUN22@users.noreply.github.com>
2023-10-27 16:38:44 +0900

    Create 과제2.py

---
+++ 과제2.py
@@ -0,0 +1,16 @@
+kg = []
+v = input("공수비를 입력하세요.") ##kg[0]
+m = input("방수압력을 입력하세요.") ##kg[1]
+a = input("평균 공기유량을 입력하시오") ##kg[2]
+w = input("평균 방수량을 입력하시오.") ##kg[7]
+b4 = input("4단붐 중량을 입력하시오, 없다면 0을 입력하세요.") ##kg[8]
+b5 = input("5단붐 중량을 입력하시오, 없다면 0을 입력하세요.") ##kg[9]
+b6 = input("6단붐 중량을 입력하시오, 없다면 0을 입력하세요.") ##kg[10]
+
+results.append(v), results.append(v1), results.append(v2)
+results.append(e1), results.append(e2)
+results.append(b1), results.append(b2), results.append(b3), results.append(b4), results.append(b5), results.append(b6)
+
+for i in results :
+    g = int(i)
+    kg.append(g)
